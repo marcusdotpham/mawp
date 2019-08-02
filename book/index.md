4fddb1d1b6dbb3c852867b1ea6b02fa0f018a52a
tanpham10 <phamtan500@gmail.com>
2019-08-02 14:23:21 +0700

    update

---
+++ book/index.md
@@ -0,0 +1,15 @@
+text
+	- string
+	- regular expression
+
+web
+	- scraping
+	- form
+
+data
+	- pdf
+	- csv
+	- excel
+	- json
+	- speadsheet
+
--- code/text/working_with_regular_expression.py
+++ code/text/working_with_regular_expression.py
@@ -1,5 +1,6 @@
 # extract phone number from text
 # extract email address from text
+# ^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*\.[0-9]{2}$
 
 import re
 import pandas as pd
